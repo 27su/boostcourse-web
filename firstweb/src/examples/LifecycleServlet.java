75ad6942134729e4a1cb2cf13c6aa3e3012bad73
김수정 <@DESKTOP-HHMRJV9>
2020-12-07 15:06:43 +0900

    lifecycle servlet - doget,dopost

--- firstweb/src/examples/LifecycleServlet.java
+++ firstweb/src/examples/LifecycleServlet.java
@@ -1,6 +1,9 @@
 package examples;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Servlet;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -40,12 +43,36 @@ public class LifecycleServlet extends HttpServlet {
 		System.out.println("destroy 호출!!");
 	}
 
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head><title>form</title></head>");
+		out.println("<body>");
+		out.println("<form method='post' action='/firstweb/LifecycleServlet'>");
+		out.println("name : <input type='text' name='name'><br>");
+		out.println("<input type='submit' value='ok'><br>");                                                 
+		out.println("</form>");
+		out.println("</body>");
+		out.println("</html>");
+		out.close();
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String name = request.getParameter("name");
+		out.println("<h1> hello " + name + "</h1>");
+		out.close();
+	}
+
 	/**
 	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		System.out.println("service호출!!");
-	}
+//	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//		System.out.println("service호출!!");
+//	}
 
 }
